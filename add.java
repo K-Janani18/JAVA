fb786cd7557328a220f9f6a667634b001c5d065e
K-Janani18 <jananii4448297@gmail.com>
2024-12-16 12:51:15 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,9 +1,16 @@
-class add
+class StarPattern
+ {
+    public static void main(String args[])
+ {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) 
 {
-public static void main(String args[])
+            for (int j = 1; j <= i; j++) 
 {
-int a=1,b=2,sum;
-sum=a+b;
-System.out.println("Sum of a and b is;"+sum);
-}
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
